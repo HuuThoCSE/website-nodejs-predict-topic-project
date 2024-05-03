90ad9bc9f5db84903e8d6b2961f0ff5b5b7c0931
HuuThoCSE <huutho.cse@gmail.com>
2024-05-03 11:51:37 +0700

    Update

--- index.js
+++ index.js
@@ -42,29 +42,51 @@ express().post('/webhook', (req, res) => {
     }
 });
 
-// Adds support for GET requests to our webhook
-express().get('/webhook', (req, res) => {
+// // Adds support for GET requests to our webhook
+// express().get('/webhook', (req, res) => {
+//
+//     // Your verify token. Should be a random string.
+//     let VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN
+//
+//     // Parse the query params
+//     let mode = req.query['hub.mode'];
+//     let token = req.query['hub.verify_token'];
+//     let challenge = req.query['hub.challenge'];
+//
+//     // Checks if a token and mode is in the query string of the request
+//     if (mode && token) {
+//
+//         // Checks the mode and token sent is correct
+//         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+//
+//             // Responds with the challenge token from the request
+//             console.log('WEBHOOK_VERIFIED');
+//             res.status(200).send(challenge);
+//
+//         } else {
+//             // Responds with '403 Forbidden' if verify tokens do not match
+//             res.sendStatus(403);
+//         }
+//     }
+// });
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", (req, res) => {
 
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+// Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
 
-    // Checks if a token and mode is in the query string of the request
+    // Check if a token and mode is in the query string of the request
     if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === process.env.MY_VERIFY_FB_TOKEN) {
+            // Respond with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
             res.status(200).send(challenge);
-
         } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
+            // Respond with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
     }
