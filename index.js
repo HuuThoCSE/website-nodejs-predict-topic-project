ba164146d9399b7e6b47f3bcb72862c7108adf46
HuuThoCSE <huutho.cse@gmail.com>
2024-05-03 11:38:53 +0700

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const path = require('path')
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 8080
 
 // Parse application/x-www-form-urlencoded
 express().use(bodyParser.urlencoded({ extended: false }))
